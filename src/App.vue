a4202492fe162c6e807a1e8fef668ec3025ac8b9
Alice Ewing <alice.ewing@mitservices.com.au>
2020-08-12 22:25:23 +1000

    V 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,73 @@
 <template>
   <div id="app">
-    <fullscreen />
-    <div class="darken">
-      <navbar />
-      <router-view/>
-      <spacer />
-    </div>
+    <container>
+        <input type="number" v-model="commons" />
+        <input type="number" v-model="uncommons" />
+        <input type="number" v-model="rares" />
+        <input type="number" v-model="mythics" />
+        <input type="checkbox" v-model="includeRandomMythics" />
+        <button @click="calculate()"> calculate </button>
+        <h3> Total Cost (USD): 
+          {{cost}}
+        </h3>
+        <h2> if you were to buy the following gem bundles: </h2>
+        <ul>
+          <li v-if="gemBundles.tiny > 0"> {{gemBundles.tiny}} $5 Gem Bundle(s) (150 gems per $1) </li>
+          <li v-if="gemBundles.small > 0"> {{gemBundles.small}} $10 Gem Bundle(s) (160 gems per $1) </li>
+          <li v-if="gemBundles.medium > 0"> {{gemBundles.medium}} $20 Gem Bundle(s) (170 gems per $1) </li>
+          <li v-if="gemBundles.big > 0"> {{gemBundles.big}} $50 Gem Bundle(s) (184 gems per $1) </li>
+          <li v-if="gemBundles.huge > 0"> {{gemBundles.huge}} $100 Gem Bundle(s) (200 gems per $1) </li>
+        </ul>
+        <h2> Price in gems:
+          {{gems}}
+        </h2>
+    </container>
   </div>
 </template>
 
 <script>
-import navbar from './components/navbar';
-import fullscreen from './components/fullscreen';
 export default {
   name: 'app',
-  components: {
-    navbar,
-    fullscreen
+  data() {
+    return {
+      commons: 0,
+      uncommons: 0,
+      rares: 0,
+      mythics: 0,
+      includeRandomMythics: false,
+      gems: 0,
+      cost: 0,
+      gemBundles: {
+        tiny: 0,
+        small: 0,
+        medium: 0,
+        big: 0,
+        huge: 0
+      }
+    }
+  },
+  methods: {
+    calculate() {
+      //TODO: assign each rarity a "pull chance" to simplify this
+      const commons = includeRandomMythics? this.common*3 : this.commons*5;
+      const uncommons = this.uncommons*5;
+      const rares = this.rares*15;
+      const mythics = this.includeRandomMythics? this.mythics*24 : this.mythics*30;
+
+      //todo: make gem bundles into {gems, price, count} to convert this into a loop
+      this.gems = Math.max(commons, uncommons, rares, mythics) * 200;
+      this.gemBundles.huge = Math.floor(this.gems/20000);
+      let remainder = this.gems % 20000;
+      this.gemBundles.big = Math.floor(remainder / 9200);
+      remainder = remainder % 9200;
+      this.gemBundles.medium = Math.floor(remainder / 3400);
+      remainder = remainder % 3400;
+      this.gemBundles.small = Math.floor(remainder / 1600);
+      remainder = remainder % 1600;
+      this.gemBundles.tiny = Math.ceil(remainder / 750);
+
+      this.cost = (this.gemBundles.huge * 100) + (this.gemBundles.big * 50) + (this.gemBundles.medium * 20) + (this.gemBundles.small * 10) + (this.gemBundles.tiny * 5);
+    }
   }
 }
 </script>
@@ -41,7 +92,12 @@ export default {
   }
 }
 html {
+  background-color: rgb(34, 34, 34);
+}
+.container {
   background-color: #E6E7ED;
+  margin-top: 3em;
+  margin-bottom: 3em;
 }
 
 button.round-big {
--- src/main.js
+++ src/main.js
@@ -1,31 +1,9 @@
 import Vue from 'vue'
-import Vuex from 'vuex';
-import ScrollActive from 'vue-scrollactive';
 import App from './App.vue'
-import router from './router'
 import './styling-components';
 
 Vue.config.productionTip = false
 
-Vue.use(Vuex);
-Vue.use(ScrollActive);
-const store = new Vuex.Store({
-  state: {
-    fullscreen: false,
-    image: ''
-  },
-  mutations: {
-    setFullscreen(state, toFS) {
-      state.fullscreen = toFS;
-    },
-    setImage(state, img) {
-      state.image = img;
-    }
-  }
-})
-
 new Vue({
-  router,
-  render: h => h(App),
-  store
+  render: h => h(App)
 }).$mount('#app')
